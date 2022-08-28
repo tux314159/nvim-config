25b30c50c6c5018ef540fba1fffe6a44fe3b9ca5
Isaac <tux314159@gmail.com>
2022-08-28 15:29:49 +0800

    Sun 28 Aug 2022 03:29:49 PM +08

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,32 +2,32 @@ return require('packer').startup(function()
   use "EdenEast/nightfox.nvim"
   use {
     "feline-nvim/feline.nvim",
-    requires={ "kyazdani42/nvim-web-devicons" },
-    config=function() require("feline").setup() end
+    requires = { "kyazdani42/nvim-web-devicons" },
+    config = function() require("feline").setup() end
   }
   use "itchyny/vim-haskell-indent"
   use "LionC/nest.nvim"
   use {
     "lewis6991/gitsigns.nvim",
-    config=function() require("gitsigns").setup() end
+    config = function() require("gitsigns").setup() end
   }
   use {
     "ms-jpq/coq_nvim",
-    branch="coq",
-    requires={ "ms-jpq/coq.artifacts" },
-    run={ ":COQdeps" }
-    config=function()
-       local vim.g.coq_settings
-       vim.g.coq_settings = {
-         auto_start = true;
-       }
+    branch = "coq",
+    requires = { "ms-jpq/coq.artifacts" },
+    run = { ":COQdeps" },
+    config = function()
+      vim.g.coq_settings = {
+        auto_start = true
+      }
+    end
   }
   use "neovim/nvim-lspconfig"
   use {
     'nvim-telescope/telescope.nvim',
-    requires={ {'nvim-lua/plenary.nvim'} }
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use { "nvim-treesitter/nvim-treesitter", run=":TSUpdate" }
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
   use "p00f/nvim-ts-rainbow"
   use "sainnhe/everforest"
   use "tpope/vim-eunuch"
