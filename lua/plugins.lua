a2e2ceef304575fbca1f0d0d82c9bdfa61969119
Isaac <tux314159@gmail.com>
2022-09-18 11:13:02 +0800

    Sun 18 Sep 2022 11:13:02 AM +08

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,12 +16,12 @@ return require('packer').startup(function()
     requires = { "ms-jpq/coq.artifacts" },
     run = { ":COQdeps" },
   }
+  use "neovimhaskell/haskell-vim"
   use "neovim/nvim-lspconfig"
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use "neovimhaskell/haskell-vim"
   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
   use "p00f/nvim-ts-rainbow"
   use "pbrisbin/vim-syntax-shakespeare"
