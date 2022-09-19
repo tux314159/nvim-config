1ca7b27ac1e441139c0a1934bba416cede02cc8b
Isaac <tux314159@gmail.com>
2022-09-19 19:59:42 +0800

    Mon 19 Sep 2022 07:59:42 PM +08

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -47,5 +47,7 @@ local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = opts.border or border
+  -- put at corner
+  opts.relative = "
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
