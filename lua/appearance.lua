66fd86d6481d0094697f9aa284f43ad9667cd511
Isaac <tux314159@gmail.com>
2022-09-27 18:39:23 +0800

    Tue Sep 27 06:39:23 PM +08 2022

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -27,4 +27,4 @@ vim.cmd("hi FloatermBorder guibg=" .. spec.palette.bg1)
 vim.cmd("hi NormalFloat guibg=" .. spec.palette.bg1)
 
 vim.cmd("hi TreesitterContext guibg=" .. spec.palette.bg2)
-require'treesitter-context'.setup({ seperator='-' })
+require'treesitter-context'.setup({ separator='-' })
