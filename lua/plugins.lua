64ed8218f273a2dfb2131b8ec14ab84826d82cf3
Isaac <tux314159@gmail.com>
2023-05-13 00:21:53 +0800

    ??

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,7 +22,7 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  --use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
   --use "nvim-treesitter/nvim-treesitter-context"
   use "p00f/nvim-ts-rainbow"
   use "pbrisbin/vim-syntax-shakespeare"
