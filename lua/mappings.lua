cc5066b163070f785359d5f6de7c567b502c81b7
Isaac <tux314159@gmail.com>
2023-06-11 22:26:18 +0800

    Sun 11 Jun 2023 10:26:18 PM +08

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,1 +1,36 @@
 local nest = require("nest")
+
+vim.g.mapleader = " "
+nest.applyKeymaps {
+	{ "<Left>", "<C-w>h" },
+	{ "<Down>", "<C-w>j" },
+	{ "<Up>", "<C-w>k" },
+	{ "<Right>", "<C-w>l" },
+	{ "<S-Left>", "<C-w>v<C-w>h" },
+	{ "<S-Down>", "<C-w>s" },
+	{ "<S-Up>", "<C-w>s<C-w>k" },
+	{ "<S-Right>", "<C-w>v" },
+
+	{ "<C-t>", "<Cmd>FloatermToggle<CR>", mode="tn" },
+	{ "<Esc>", "<C-\\><C-n>", mode="t" },
+
+	-- special lsp, not inside leader
+	{ "K", "<cmd>lua vim.lsp.buf.hover()<CR>" },
+	{ "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>" },
+
+	{ "<leader>", {
+		{ "l", {  -- lsp
+			{ "D", "<cmd>lua vim.lsp.buf.declaration()<CR>" },
+			{ "d", "<cmd>lua vim.lsp.buf.definition()<CR>" },
+			{ "r", "<cmd>lua vim.lsp.buf.rename()<CR>" },
+			{ "a", "<cmd>lua vim.lsp.buf.code_action()<CR>" },
+		}},
+
+		{ "f", { -- telescope
+			{ "f", "<cmd>lua require('telescope.builtin').find_files()<cr>" },
+			{ "g", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
+			{ "b", "<cmd>lua require('telescope.builtin').buffers()<cr>" },
+			{ "h", "<cmd>lua require('telescope.builtin').help_tags()<cr>" },
+		}}
+	}}
+}
