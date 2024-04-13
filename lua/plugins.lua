d78e4c8d1f00acbe6a94f95a1a5b70c6425f2a34
Isaac <tux314159@gmail.com>
2024-04-13 11:30:25 +0800

    Sat 13 Apr 2024 11:30:25 AM +08

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,4 +26,12 @@ return require('packer').startup(function()
   use "tpope/vim-scriptease"
   use "voldikss/vim-floaterm"
   use "wbthomason/packer.nvim"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-vsnip"
+  use "hrsh7th/vim-vsnip"
+
 end)
