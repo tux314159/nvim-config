106d855e9ceb20794ca1538914971cbb050afd6a
Tux <tux314159@gmail.com>
2024-07-02 13:26:38 +0800

    Tue 02 Jul 2024 01:26:37 PM +08

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -19,6 +19,6 @@ require'nvim-treesitter.configs'.setup {
 }
 
 -- Folding
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
-vim.opt.foldmethod = "expr"
+--vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+--vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+--vim.opt.foldmethod = "expr"
