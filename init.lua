9476c3c8006d2d4e773780c15b954314d55a598b
Tux <tux314159@gmail.com>
2024-07-21 19:14:43 +0800

    update config

--- init.lua
+++ init.lua
@@ -1,14 +1,17 @@
+local package = require("package")
 
 local config_mods = {"plugins", "ide", "appearance", "mappings", "options"}
 local config_mods_loaded = {}
 for _, mod in pairs(config_mods) do
-  config_mods_loaded[vim.fn.stdpath("config") .. "/" .. mod .. ".lua"] = require(mod)
+  config_mods_loaded[mod] = require(mod)
 end
+
 
 -- Push config changes on file change, and reload changed files
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { vim.fn.expand("~") .. "/.config/nvim/*" },
   callback = function(ev)
+    print(ev.match)
     config_mods_loaded[ev.match].loaded = false
     config_mods_loaded[ev.match] = require(ev.match)
 
