48fe81cc47f2027c733b5c72b153f3b1bdc60c5f
Tux <tux314159@gmail.com>
2024-07-21 21:10:58 +0800

    update config

--- lua/config-telescope.lua
+++ lua/config-telescope.lua
@@ -7,15 +7,7 @@ local config = {
   },
 
   keymaps = {
-
-    { "<leader>", {
-      { "f", { -- telescope
-        { "f", "<cmd>lua require('telescope.builtin').find_files()<cr>" },
-        { "g", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
-        { "b", "<cmd>lua require('telescope.builtin').buffers()<cr>" },
-        { "h", "<cmd>lua require('telescope.builtin').help_tags()<cr>" },
-      }},
-    }},
+    { "n", "<leader>ff", function() require("telescope.builtin").find_files() end },
   }
 }
 
