c82f01d9243d0f5143a4d82dc19d9ad513a63c34
Tux <tux314159@gmail.com>
2024-07-21 20:24:40 +0800

    update config

--- lua/ide.lua
+++ lua/ide.lua
@@ -50,17 +50,21 @@ local config = {
         nvim_lsp.lua_ls.setup {
           on_init = function(client)
             local path = client.workspace_folders[1].name
-            if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            if vim.loop.fs_stat(path.."/.luarc.json") or
+              vim.loop.fs_stat(path.."/.luarc.jsonc") then
               return
             end
 
-            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-              runtime = { version = "LuaJIT" },
-              workspace = {
-                checkThirdParty = false,
-                library = vim.api.nvim_get_runtime_file("", true)
-              }
-            })
+            client.config.settings.Lua = vim.tbl_deep_extend(
+              'force',
+              client.config.settings.Lua,
+              {
+                runtime = { version = "LuaJIT" },
+                workspace = {
+                  checkThirdParty = false,
+                  library = vim.api.nvim_get_runtime_file("", true)
+                }
+              })
           end,
           settings = {
             Lua = {}
