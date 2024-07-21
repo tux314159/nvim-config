4c9b2d8a2a8c210ce85eafaeb9af04a43e88707a
Tux <tux314159@gmail.com>
2024-07-21 19:35:00 +0800

    update config

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ for _, mod in pairs(config_mods) do
   config_mods_loaded[mod] = require(mod)
 end
 
-
 -- Push config changes on file change, and reload changed files
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { vim.fn.expand("~") .. "/.config/nvim/*" },
