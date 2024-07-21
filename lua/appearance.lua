4fdf25111fcfd4167684d88aba3e0acce3e48205
Tux <tux314159@gmail.com>
2024-07-21 16:41:52 +0800

    Sun 21 Jul 2024 04:41:52 PM +08

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -1,26 +1,6 @@
 vim.o.termguicolors = true
 
-require('nightfox').setup({
-  options = {
-    transparent = false,
-    styles = {
-      comments = "italic",
-      keywords = "bold",
-      types = "italic,bold",
-    },
-    inverse = {
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-  },
-  palettes = {
-    duskfox = {
-      -- bg1 = "#272822"
-    }
-  }
-})
-vim.cmd("colorscheme duskfox")
+vim.cmd[[colorscheme tokyonight]]
 
 local spec = require('nightfox.spec').load("duskfox")
 vim.cmd("hi FloatermBorder guibg=" .. spec.palette.bg1)
