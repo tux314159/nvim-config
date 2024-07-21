20caed40a79dcd2b3151466a1d0d7950d9955dcb
Tux <tux314159@gmail.com>
2024-07-21 20:41:21 +0800

    update config

--- lua/ide.lua
+++ lua/ide.lua
@@ -9,8 +9,7 @@ local config = {
         require'nvim-treesitter.configs'.setup({
           sync_install = false,
           auto_install = true,
-
-          ignore_install = { "" },
+          ignore_install = { },
           ensure_installed = "all",
 
           highlight = {
