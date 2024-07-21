d40a65787abe1c7aad9c11e9136c9c171f42bae4
Tux <tux314159@gmail.com>
2024-07-21 18:33:40 +0800

    update config

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
 require("plugins")
-
 require("ide")
-
 require("appearance")
 require("mappings")
 require("options")
+
+-- Push config changes every time
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { vim.fn.expand("~") .. "/.config/nvim/*" },
+  callback = function(_)
+    local with_config_dir = { cwd = vim.fn.expand("~") ..  "/.config/nvim" }
+    vim.system({"git", "add", "."}, with_config_dir):wait()
+    vim.system({"git", "commit", "-m", "update config"}, with_config_dir):wait()
+    vim.system({"git", "push"}, with_config_dir)
+  end,
+})
