6ed6fa1b1ec519cce61154da7d132f02482a5dcd
Tux <tux314159@gmail.com>
2024-07-21 22:20:46 +0800

    update config

--- lua/config-appearance.lua
+++ lua/config-appearance.lua
@@ -35,18 +35,6 @@ local config = {
       shortmess = "c",
       signcolumn = "yes:2",
       scrolloff = 5,
-mouse = "a",
-splitbelow = true,
-splitright = true,
-undofile = true,  -- persistent undo
-updatetime = 300,
-
-shiftwidth = 4,
-tabstop = 4,
-expandtab = true,
-autoindent = true,
-cinoptions = "l1:0",
-
     },
   },
 }
