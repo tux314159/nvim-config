da89dde999e295d5ba5922d36ce466ca3b55bfe5
Tux <tux314159@gmail.com>
2024-07-21 21:11:33 +0800

    update config

--- lua/config-telescope.lua
+++ lua/config-telescope.lua
@@ -8,6 +8,9 @@ local config = {
 
   keymaps = {
     { "n", "<leader>ff", function() require("telescope.builtin").find_files() end },
+    { "n", "<leader>fg", function() require("telescope.builtin").live_grep() end },
+    { "n", "<leader>fb", function() require("telescope.builtin").buffers() end },
+    { "n", "<leader>fh", function() require("telescope.builtin").help_tags() end },
   }
 }
 
