ddbe155d12185e8f082404c17ffaa875184e23e6
Tux <tux314159@gmail.com>
2024-07-21 17:39:17 +0800

    Sun 21 Jul 2024 05:39:17 PM +08

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,4 +1,41 @@
--- Autocomplete
+-- LSP
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+nvim_lsp = require("lspconfig")
+local servers = { "clangd", "pyright", "tsserver", "bashls", "hls", "csharp_ls", "lua_ls" }
+for k, v in pairs(servers) do
+	nvim_lsp[v].setup({capabilities = capabilities})
+end
+
+nvim_lsp.rls.setup {
+  settings = {
+    rust = {
+      unstable_features = true,
+      build_on_save = false,
+      all_features = true,
+    },
+  },
+}
+
+vim.diagnostic.config({
+  virtual_text = false,
+  float = {
+    scope = "cursor",
+  }
+})
+vim.cmd("autocmd CursorHold * lua vim.diagnostic.open_float { focusable = false }")
+vim.cmd("autocmd CursorHoldI * lua vim.diagnostic.open_float { focusable = false }")
+
+
+-- LSP UI
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+-- Completion
+
 local cmp = require'cmp'
 
 cmp.setup({
@@ -37,37 +74,3 @@ cmp.setup.cmdline(':', {
   matching = { disallow_symbol_nonprefix_matching = false }
 })
 
--- Set up lspconfig
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-nvim_lsp = require("lspconfig")
-local servers = { "clangd", "pyright", "tsserver", "bashls", "hls", "csharp_ls" }
-for k, v in pairs(servers) do
-	nvim_lsp[v].setup({capabilities = capabilities})
-end
-
-nvim_lsp.rls.setup {
-  settings = {
-    rust = {
-      unstable_features = true,
-      build_on_save = false,
-      all_features = true,
-    },
-  },
-}
-
-vim.diagnostic.config({
-  virtual_text = false,
-  float = {
-    scope = "cursor",
-  }
-})
-vim.cmd("autocmd CursorHold * lua vim.diagnostic.open_float { focusable = false }")
-vim.cmd("autocmd CursorHoldI * lua vim.diagnostic.open_float { focusable = false }")
-
-
--- LSP UI
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-  opts = opts or {}
-  return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
