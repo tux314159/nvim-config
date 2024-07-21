d1636d9d37872a11e2a1002e26b6ca900d8f3a54
Tux <tux314159@gmail.com>
2024-07-21 19:15:27 +0800

    update config

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-local package = require("package")
-
 local config_mods = {"plugins", "ide", "appearance", "mappings", "options"}
 local config_mods_loaded = {}
 for _, mod in pairs(config_mods) do
@@ -11,10 +9,6 @@ end
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { vim.fn.expand("~") .. "/.config/nvim/*" },
   callback = function(ev)
-    print(ev.match)
-    config_mods_loaded[ev.match].loaded = false
-    config_mods_loaded[ev.match] = require(ev.match)
-
     local with_config_dir = { cwd = vim.fn.expand("~") ..  "/.config/nvim" }
     vim.system({"git", "add", "."}, with_config_dir):wait()
     vim.system({"git", "commit", "-m", "update config"}, with_config_dir):wait()
