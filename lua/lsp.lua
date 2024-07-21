474745920aaaa019a64268a10f12f96e34087290
Tux <tux314159@gmail.com>
2024-07-21 17:53:01 +0800

    Sun 21 Jul 2024 05:53:01 PM +08

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -26,18 +26,11 @@ vim.diagnostic.config({
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT'
-      },
-      -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
         library = {
           vim.env.VIMRUNTIME
         },
-        -- library = vim.api.nvim_get_runtime_file("", true)
       }
     })
   end,
