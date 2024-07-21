06392006e1eb7b484d3d2deba31b09e8f98e3093
Tux <tux314159@gmail.com>
2024-07-21 17:57:45 +0800

    Sun 21 Jul 2024 05:57:45 PM +08

---
+++ lua/ide.lua
@@ -0,0 +1,26 @@
+-- Treesitter
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "all",
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+
+  --indent = {
+    --enable = true,
+  --},
+
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" },
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
+
+-- Folding
+--vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+--vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+vim.opt.foldmethod = "marker"
