a3ceba4c7564fb376b9d852451125cb88f11a996
Tux <tux314159@gmail.com>
2024-07-21 17:54:46 +0800

    Sun 21 Jul 2024 05:54:46 PM +08

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -8,36 +8,36 @@ for k, v in pairs(servers) do
 end
 
 nvim_lsp.lua_ls.setup {
-  diagnostics = {
-    -- Get the language server to recognize the `vim` global
-    globals = { "vim" },
-  },
-  workspace = {
-    -- Make the server aware of Neovim runtime files
-    library = vim.api.nvim_get_runtime_file("", true),
-  },
-}
-
-vim.diagnostic.config({
- on_init = function(client)
+  on_init = function(client)
     local path = client.workspace_folders[1].name
     if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
       return
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
         library = {
           vim.env.VIMRUNTIME
-        },
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
       }
     })
   end,
   settings = {
     Lua = {}
   }
-})
+}
 
 vim.cmd("autocmd CursorHold * lua vim.diagnostic.open_float { focusable = false }")
 vim.cmd("autocmd CursorHoldI * lua vim.diagnostic.open_float { focusable = false }")
