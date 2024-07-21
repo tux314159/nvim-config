84f09228c9ac6e76aaf808a79ffe967c1fd4b3bd
Tux <tux314159@gmail.com>
2024-07-21 19:33:07 +0800

    update config

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -1,7 +1,5 @@
 local config = {
-  plugins = {
-    { "tokyonight" }
-  },
+  plugins = {"tokyonight"},
 
   options = {
     o = {
@@ -17,7 +15,7 @@ local config = {
     terminal_colors = true,
   }),
 
-  vim.cmd[[colorscheme tokyonight]]
+  vim.cmd.colorscheme("tokyonight")
 
   require("toggleterm").setup({
     shade_terminals = true,
