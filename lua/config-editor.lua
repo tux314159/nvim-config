a7a524923c9f3934b542ad07349992d845a440ad
Tux <tux314159@gmail.com>
2024-07-21 22:21:30 +0800

    update config

---
+++ lua/config-editor.lua
@@ -0,0 +1,19 @@
+local config = {
+  options = {
+    o = {
+      mouse = "a",
+      splitbelow = true,
+      splitright = true,
+      undofile = true,  -- persistent undo
+      updatetime = 300,
+
+      shiftwidth = 4,
+      tabstop = 4,
+      expandtab = true,
+      autoindent = true,
+      cinoptions = "l1:0",
+    }
+  }
+}
+
+return config
