c9836b47ef561f1425bdc9847be1931ee095e11d
Tux <tux314159@gmail.com>
2024-07-21 17:34:12 +0800

    Sun 21 Jul 2024 05:34:12 PM +08

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -11,7 +11,7 @@ require("tokyonight").setup({
 vim.cmd[[colorscheme tokyonight]]
 
 require("toggleterm").setup({
-  shade_terminals = false,
+  shade_terminals = true,
   direction = "float",
   float_opts = {
     border = "none",
