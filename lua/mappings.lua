f8d28d5881b3c47e5534cb4b6225e3f3258229d6
Tux <tux314159@gmail.com>
2024-07-21 20:52:26 +0800

    update config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -36,15 +36,19 @@ local config = {
           }},
         }
 
-
         -- weird snippet stuff
         vim.cmd("imap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'")
         vim.cmd("smap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'")
         vim.cmd("imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'")
         vim.cmd("smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'")
       end
-    }
-  }
+    },
+  },
+  options = {
+    g = {
+      mapleader = " ",
+    },
+  },
 }
 
 vim.g.mapleader = " "
