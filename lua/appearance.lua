26336cb72b1c673d0f4423b761265e272f8aa573
Tux <tux314159@gmail.com>
2024-07-21 17:00:14 +0800

    Sun 21 Jul 2024 05:00:14 PM +08

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -4,7 +4,7 @@ vim.o.background = "dark"
 require("tokyonight").setup({
   style = "moon",
   transparent = true,
-  terminal_colors = false,
+  terminal_colors = true,
 })
 
 vim.cmd[[colorscheme tokyonight]]
