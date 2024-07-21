3f000823f1993170f21fb6c1e05d1c6f23711a0c
Tux <tux314159@gmail.com>
2024-07-21 17:58:13 +0800

    Sun 21 Jul 2024 05:58:13 PM +08

--- lua/ide.lua
+++ lua/ide.lua
@@ -24,3 +24,79 @@ require'nvim-treesitter.configs'.setup {
 --vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 --vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
 vim.opt.foldmethod = "marker"
+
+-- LSP
+--
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+nvim_lsp = require("lspconfig")
+local servers = { "clangd", "pyright", "tsserver", "bashls", "hls", "csharp_ls", "lua_ls" }
+for k, v in pairs(servers) do
+	nvim_lsp[v].setup({capabilities = capabilities})
+end
+
+nvim_lsp.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = { version = "LuaJIT" },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = { vim.env.VIMRUNTIME }
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}
+
+vim.cmd("autocmd CursorHold * lua vim.diagnostic.open_float { focusable = false }")
+vim.cmd("autocmd CursorHoldI * lua vim.diagnostic.open_float { focusable = false }")
+
+-- Completion
+
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
