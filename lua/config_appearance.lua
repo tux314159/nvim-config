02e4a86592c39371b2c5cef27f1650dd6fc3b2b3
Tux <tux314159@gmail.com>
2024-07-22 19:02:33 +0800

    update config

--- lua/config_appearance.lua
+++ lua/config_appearance.lua
@@ -1,41 +1,41 @@
 local config = {
-  plugins = {
-    {
-      "rebelot/kanagawa.nvim", config = function ()
-        require"kanagawa".setup({
-          transparent = true,
+	plugins = {
+		{
+			"rebelot/kanagawa.nvim", config = function ()
+				require"kanagawa".setup({
+					transparent = true,
 					theme = "lotus",
-        })
-        vim.cmd.colorscheme("kanagawa")
-      end
-    },
-    {
-      "akinsho/toggleterm.nvim", config = function()
-        require"toggleterm".setup({
-          shade_terminals = true,
-          direction = "float",
-          float_opts = {
-            border = "none",
-            winblend = 20,
-            title_pos = "center",
-          },
-        })
-      end
-    },
-  },
+				})
+				vim.cmd.colorscheme("kanagawa")
+			end
+		},
+		{
+			"akinsho/toggleterm.nvim", config = function()
+				require"toggleterm".setup({
+					shade_terminals = true,
+					direction = "float",
+					float_opts = {
+						border = "none",
+						winblend = 20,
+						title_pos = "center",
+					},
+				})
+			end
+		},
+	},
 
-  options = {
-    o = {
-      termguicolors = true,
-      background = "dark",
-      winblend = 20,
-      cmdheight = 1,
-      number = true,
-      shortmess = "c",
-      signcolumn = "yes:2",
-      scrolloff = 5,
-    },
-  },
+	options = {
+		o = {
+			termguicolors = true,
+			background = "dark",
+			winblend = 20,
+			cmdheight = 1,
+			number = true,
+			shortmess = "c",
+			signcolumn = "yes:2",
+			scrolloff = 5,
+		},
+	},
 }
 
 return config
