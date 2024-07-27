6ab6da1965b72b08a31528e52da1a0d704a0c8c3
Tux <tux314159@gmail.com>
2024-07-27 13:18:13 +0800

    update config

--- lua/config_appearance.lua
+++ lua/config_appearance.lua
@@ -19,7 +19,7 @@ local config = {
 					shade_terminals = true,
 					direction = "float",
 					float_opts = {
-						border = "none",
+						border = "rounded",
 						winblend = 20,
 						title_pos = "center",
 					},
