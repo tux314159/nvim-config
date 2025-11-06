9644f45280c5a8c3aa48b2311f52d310ae0cefe5
Isaac Yeo <tux314159@gmail.com>
2025-11-06 12:56:21 +0800

    update config

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ set 'wildmode' 'full:longest'
 -- Omnicomplete
 set 'completeopt' 'longest,menuone'
 function set_omni_keymap(k, expr)
-  vim.keymap.set('i', k, function() return vim.fn.pumvisible() == 1 and expr or k end, { expr = true })
+  vim.keymap.set('i', k, function() return vim.fn.pumvisible() == 1 and expr or k end, { expr = true, silent = true })
 end
   
 set_omni_keymap('<CR>', '<C-y>')
