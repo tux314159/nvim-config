9e62e476d0a692422d0ba33870652e7d0a28d220
Isaac Yeo <tux314159@gmail.com>
2025-11-06 15:17:23 +0800

    update config

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -4,9 +4,4 @@ set'local' 'expandtab' (true)
 
 ftp = require'ftplib'
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-  buffer = 0,
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end,
-})
+ftp.fmt_autocmd()
