97f67cd0a7a814e8324dcafb9ba0f64539f27df2
Isaac Yeo <tux314159@gmail.com>
2025-11-06 15:24:32 +0800

    update config

--- after/ftplugin/cabal.lua
+++ after/ftplugin/cabal.lua
@@ -3,4 +3,4 @@ set'local' 'shiftwidth' (4)
 set'local' 'expandtab' (true)
 
 lib = require'lib'
-lib.fmt_autocmd(function() vim.cmd '%!cabal-fmt' end)
+lib.buflocal_autofmt(function() vim.cmd '%!cabal-fmt' end)
