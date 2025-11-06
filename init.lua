a2dd0a3430a71620b4a9cb30bffebbe670871763
Isaac Yeo <tux314159@gmail.com>
2025-11-06 12:59:01 +0800

    update config

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ end
 set_omni_keymap('<CR>', '<C-y>')
 set_omni_keymap('<Esc>', '<C-e>')
 
-vim.keymap.set('i', '<Tab>',
+vim.keymap.set('i', '<C-Space>',
   function() return vim.fn.pumvisible() == 1 and '<C-n>' or '<C-x><C-o>' end,
   { expr = true, silent = true }
 )
