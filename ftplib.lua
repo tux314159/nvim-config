b98e9df8844e2304cbd59cb24ce2a7ea55c05789
Isaac Yeo <tux314159@gmail.com>
2025-11-06 15:15:53 +0800

    update config

--- ftplib.lua
+++ ftplib.lua
@@ -9,9 +9,7 @@ function M.fmt_autocmd(fmt)
 
   vim.api.nvim_create_autocmd('BufWritePre', {
     buffer = 0,
-    callback = function()
-      vim.lsp.buf.format({ async = false })
-    end,
+    callback = fmt
   })
 end
 
