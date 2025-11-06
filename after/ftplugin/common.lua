fd88a619cf797e7d488034eaf4970ccdc406d251
Isaac Yeo <tux314159@gmail.com>
2025-11-06 15:13:31 +0800

    update config

--- after/ftplugin/common.lua
+++ after/ftplugin/common.lua
@@ -1,4 +1,6 @@
-return {
+local M = {}
+
+function M.fmt_autocmd()
   vim.api.nvim_create_autocmd('BufWritePre', {
     buffer = 0,
     callback = function()
@@ -6,3 +8,5 @@ return {
     end,
   })
 }
+
+return M
