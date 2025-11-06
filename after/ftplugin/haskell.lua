078a08a82a427e4ff2791fd608d81a83ca54090f
Isaac Yeo <tux314159@gmail.com>
2025-11-06 15:22:47 +0800

    update config

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -2,6 +2,6 @@ set = require'set'
 set'local' 'shiftwidth' (2)
 set'local' 'expandtab' (true)
 
-lib = require'lib'
+lib = require'thing'
 
 lib.fmt_autocmd()
---
+++ after/ftplugin/thing.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function M.fmt_autocmd(fmt)
+  if not fmt then
+    fmt = function()
+      vim.lsp.buf.format({ async = false })
+    end
+  end
+
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = 0,
+    callback = fmt
+  })
+end
+
+return M
