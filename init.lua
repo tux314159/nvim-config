f4bfe21a8638e4daea74e8541312a07ce602b869
Isaac Yeo <tux314159@gmail.com>
2025-11-06 11:09:28 +0800

    update config

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ set 'splitright' (true)
 
 -- File tree
 vim.g.netrw_liststyle = 3
-vim.keymap.set('n', '<C-,>', '<Cmd>Lex<CR>')
+vim.keymap.set('n', '<C-,>', '<Cmd>Lexplore<CR>')
 
 vim.api.nvim_create_autocmd('CursorMoved', {
   callback = function(_)
