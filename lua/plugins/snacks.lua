68054761f7baed211a171dec71579a7b9b634751
Isaac Yeo <tux314159@gmail.com>
2026-03-21 08:59:06 +0800

    update config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -18,6 +18,6 @@
     scope = { enabled = true },
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
-    words = { enabled = true }
-  }
+    words = { enabled = true },
+  },
 }
