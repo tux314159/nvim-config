8ccf07d498a0f7c23adcb895179445fd86655069
Isaac Yeo <tux314159@gmail.com>
2026-03-21 09:00:44 +0800

    update config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,15 +4,12 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    animate = { enabled = true },
     bigfile = { enabled = true },
     dashboard = { enabled = true },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
-    picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
